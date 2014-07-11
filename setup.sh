4047d92903f075293197fa0a623fd79baa6fb1a4
casen davis <casen@casens-MacBook-Pro.local>
2014-07-11 13:16:20 -0700

    adding setup script to create symlinks

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s vim ~/.vim
+ln -s vimrc ~/.vimrc
+ln -s ackrc ~/.ackrc
+ln -s bash ~/.bash
+ln -s bash_profile ~/.bash_profile
+ln -s gitconfig ~/.gitconfig
+ln -s gitignore_global ~/.gitignore_global
